4a086dfc545765b17da25d82f84943c15eb8e7db
ringyuki <hikarinagi@outlook.com>
2026-02-18 16:40:11 +0800

    fix(config): update ecosystem configuration to use node_args for environment file

--- apps/backend/ecosystem.config.cjs
+++ apps/backend/ecosystem.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      env_file: '.env',
+      node_args: '--env-file=.env',
       env_production: {
         NODE_ENV: 'production',
       },
