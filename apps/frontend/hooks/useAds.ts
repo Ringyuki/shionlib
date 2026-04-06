cb68cbd2a92bc665e02f7bb2a011d346ac18d1dd
ringyuki <hikarinagi@outlook.com>
2026-04-06 12:12:27 +0800

    feat(ads): implement caching for ad placements in useAds hook
    - Added caching mechanism to the useAds hook to store fetched ads in localStorage with a time-to-live (TTL) of 30 minutes.
    - Implemented logic to check for cached ads and their expiration before fetching new ads, improving performance and reducing unnecessary API calls.

--- apps/frontend/hooks/useAds.ts
+++ apps/frontend/hooks/useAds.ts
@@ -4,11 +4,46 @@ import { useState, useEffect } from 'react'
 import { Ad } from '@/interfaces/site/ad.interface'
 import { fetchAdsByPlacement } from '@/libs/ad/fetch-ads'
 
+const TTL = 30 * 60_000
+
+interface CachedAds {
+  expire: number
+  ads: Ad[]
+}
+
+const getKey = (placement: string) => `shionlib:ads:${placement}`
+const getValue = (ads: Ad[]): CachedAds => {
+  return {
+    expire: Date.now() + TTL,
+    ads,
+  }
+}
+
+const isExpire = (cache: CachedAds) => {
+  return cache.expire < Date.now()
+}
+
 export const useAds = (placement: string) => {
   const [ads, setAds] = useState<Ad[]>([])
 
   useEffect(() => {
-    fetchAdsByPlacement(placement).then(setAds)
+    const cached = localStorage.getItem(getKey(placement))
+    if (cached) {
+      const cache = JSON.parse(cached) as CachedAds
+      if (isExpire(cache)) {
+        fetchAdsByPlacement(placement).then(ads => {
+          setAds(ads)
+          ads.length && localStorage.setItem(getKey(placement), JSON.stringify(getValue(ads)))
+        })
+      } else {
+        setAds(cache.ads)
+      }
+      return
+    }
+    fetchAdsByPlacement(placement).then(ads => {
+      setAds(ads)
+      ads.length && localStorage.setItem(getKey(placement), JSON.stringify(getValue(ads)))
+    })
   }, [placement])
 
   return ads
