f680401f485f9ffaff920b95b268ba6317d3a2ed
ringyuki <hikarinagi@outlook.com>
2026-06-11 00:50:38 +0800

    feat: add cloud.touchgaloss.com to allowed hosts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -66,6 +66,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'cloud.touchgaloss.com',
+      },
+      {
+        protocol: 'https',
         hostname: process.env.NEXT_PUBLIC_SHIONLIB_IMAGE_BED_HOST!,
       },
     ],
